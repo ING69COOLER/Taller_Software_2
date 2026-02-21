192c63d03fb2d2462c13f32be5be86e3571c6da6
Manuel Pineda Varela <159482318+ING69COOLER@users.noreply.github.com>
2026-02-21 14:49:50 -0500

    first commit

---
+++ src/main/java/com/uniquindio/edu/co/Taller_Software_2/TallerSoftware2Application.java
@@ -0,0 +1,13 @@
+package com.uniquindio.edu.co.Taller_Software_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerSoftware2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerSoftware2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniquindio/edu/co/Taller_Software_2/TallerSoftware2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniquindio.edu.co.Taller_Software_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerSoftware2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
